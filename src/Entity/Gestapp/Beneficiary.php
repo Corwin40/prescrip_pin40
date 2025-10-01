73c20bb0bdd17cf4f9e96574fa67012645d8ffa1
OpenPixl <xavier.burke@gmail.com>
2025-10-01 18:02:51 +0200

    DEV | Création entity beneficiary

---
+++ src/Entity/Gestapp/Beneficiary.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity\Gestapp;
+
+use App\Repository\Gestapp\BeneficiaryRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BeneficiaryRepository::class)]
+class Beneficiary
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 6)]
+    private ?string $civility = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $gender = null;
+
+    #[ORM\Column(length: 25, nullable: true)]
+    private ?string $ageGroup = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $professionnalStatus = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $createdAt = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getCivility(): ?string
+    {
+        return $this->civility;
+    }
+
+    public function setCivility(string $civility): static
+    {
+        $this->civility = $civility;
+
+        return $this;
+    }
+
+    public function getGender(): ?string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getAgeGroup(): ?string
+    {
+        return $this->ageGroup;
+    }
+
+    public function setAgeGroup(?string $ageGroup): static
+    {
+        $this->ageGroup = $ageGroup;
+
+        return $this;
+    }
+
+    public function getProfessionnalStatus(): ?string
+    {
+        return $this->professionnalStatus;
+    }
+
+    public function setProfessionnalStatus(string $professionnalStatus): static
+    {
+        $this->professionnalStatus = $professionnalStatus;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
