81251f3e0396028f8226626008db4bdc56121a03
jefflearning40 <jfld791000@gmail.com>
2025-10-06 11:53:12 +0200

    page acceuil mentions et prezDon

--- src/Controller/Webapp/PublicController.php
+++ src/Controller/Webapp/PublicController.php
@@ -11,8 +11,20 @@ final class PublicController extends AbstractController
     #[Route('/', name: 'app_webapp_public_accueil')]
     public function index(): Response
     {
-        return $this->render('webapp/public/acceuil.html.twig', [
-            'controller_name' => 'PublicController',
-        ]);
+        
+        return $this->render('webapp/public/accueil.html.twig');
+       
+    }
+
+    #[Route('/mentions', name: 'app_webapp_public_mentions')]
+    public function mentions(): Response
+    {
+        return $this->render('webapp/public/mentions.html.twig');
+    }
+
+    #[Route('/prezDon', name: 'app_webapp_public_prezDon')]
+    public function prezDon(): Response
+    {
+        return $this->render('webapp/public/prezDon.html.twig');
     }
 }
