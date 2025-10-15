7176e02c54c0910ebf30130c65af07f7bf974e4e
jefflearning40 <jfld791000@gmail.com>
2025-10-15 18:00:46 +0200

    tranche age radio

--- src/Form/Gestapp/BeneficiaryType.php
+++ src/Form/Gestapp/BeneficiaryType.php
@@ -49,7 +49,7 @@ class BeneficiaryType extends AbstractType
             ])
 
             ->add('professionnalStatus', ChoiceType::class, [
-                'label' => 'Situation',
+                'label' => 'Statut professionnel',
                 'choices' => [
                     'scolaire/Étudiant' => 'scolaire/etudiant',
                     'En emploi ou en formation' => 'employe/formation',
