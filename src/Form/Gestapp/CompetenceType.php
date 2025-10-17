a40807212c593dd8f4e9ba58f4449c77d1764ff0
jefflearning40 <jfld791000@gmail.com>
2025-10-17 17:34:36 +0200

    refacto competence new

--- src/Form/Gestapp/CompetenceType.php
+++ src/Form/Gestapp/CompetenceType.php
@@ -6,6 +6,7 @@ use App\Entity\Gestapp\Beneficiary;
 use App\Entity\Gestapp\Competence;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +15,54 @@ class CompetenceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('compBase')
-            ->add('compDesk')
-            ->add('compInternet')
-            ->add('compEmail')
+            ->add('compBase', ChoiceType::class, [
+                'label' => 'competence',
+                'choices' => [
+                    'Acquis' => 'acquis',
+                    'En cours' => 'encours',
+                    'Non acquis' => 'Nonacquis',
+
+                ],
+                'placeholder' => 'Veuillez choisir',
+                'required' => true,
+            ])
+            ->add('compDesk', ChoiceType::class, [
+                'label' => 'competence',
+                'choices' => [
+                    'Acquis' => 'acquis',
+                    'En cours' => 'encours',
+                    'Non acquis' => 'Nonacquis',
+                ],
+                 'placeholder' => 'Veuillez choisir',
+                'required' => true,
+            ])
+            ->add('compInternet', ChoiceType::class, [
+        'label' => 'competence',
+        'choices' => [
+            'Acquis' => 'acquis',
+            'En cours' => 'encours',
+            'Non acquis' => 'Nonacquis',
+        ],
+             'placeholder' => 'Veuillez choisir',
+             'required' => true,
+    ])
+
+
+
+            ->add('compEmail', ChoiceType::class, [
+        'label' => 'competence',
+        'choices' => [
+            'Acquis' => 'acquis',
+            'En cours' => 'encours',
+            'Non acquis' => 'Nonacquis',
+        ],
+             'placeholder' => 'Veuillez choisir',
+             'required' => true,
+    ])
+
+
+
+
             ->add('isAutoEva')
             ->add('isDigComp1')
             ->add('isDigComp2')
