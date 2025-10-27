7daa0e5f73af50dafb1c37f90e25a3b4e1c086a6
OpenPixl <xavier.burke@gmail.com>
2025-10-27 17:30:10 +0100

    DEV | Première esquisse espace dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\Gestapp\EquipmentRepository;
+use App\Repository\Gestapp\PrescriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_admin_dashboard_index')]
-    public function index(): Response
+    public function index(EquipmentRepository $equipmentRepository, PrescriptionRepository $prescriptionRepository): Response
     {
+        $equipments = $equipmentRepository->findAll();
+        $prescriptions = $prescriptionRepository->findAll();
+
         return $this->render('admin/login_dashboard/index.html.twig', [
-            'controller_name' => 'LoginDashboardController',
+            'equipments' => $equipments,
+            'prescriptions' => $prescriptions,
         ]);
     }
 }
