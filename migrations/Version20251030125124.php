ef9adac8e8b0dfb04a924e96f4bb96d9949f636b
OpenPixl <xavier.burke@gmail.com>
2025-10-30 14:59:47 +0100

    DEV | Modification Vue Form Prescription

--- migrations/Version20251030125124.php
+++ migrations/Version20251030125124.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251030092820 extends AbstractMigration
+final class Version20251030125124 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -32,12 +32,12 @@ final class Version20251030092820 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE competence CHANGE is_auto_eva_end is_auto_eval_end TINYINT(1) NOT NULL');
         $this->addSql('ALTER TABLE beneficiary ADD beneficiary_competences_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE beneficiary ADD CONSTRAINT FK_7ABF446A4923D850 FOREIGN KEY (beneficiary_competences_id) REFERENCES competence (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_7ABF446A4923D850 ON beneficiary (beneficiary_competences_id)');
         $this->addSql('ALTER TABLE prescription DROP FOREIGN KEY FK_1FBFB8D915761DAB');
         $this->addSql('DROP INDEX UNIQ_1FBFB8D915761DAB ON prescription');
         $this->addSql('ALTER TABLE prescription DROP competence_id');
-        $this->addSql('ALTER TABLE competence CHANGE is_auto_eva_end is_auto_eval_end TINYINT(1) NOT NULL');
     }
 }
