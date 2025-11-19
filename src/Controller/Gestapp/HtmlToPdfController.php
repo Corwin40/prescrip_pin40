8789b9da8ce8a22488658ee0496fce88e0c3a216
jefflearning40 <jfld791000@gmail.com>
2025-11-19 17:58:05 +0100

    refacto htmltopdf controller

--- src/Controller/Gestapp/HtmlToPdfController.php
+++ src/Controller/Gestapp/HtmlToPdfController.php
@@ -2,40 +2,18 @@
 
 namespace App\Controller\Gestapp;
 
+use App\Entity\Gestapp\Prescription;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HtmlToPdfController extends AbstractController
 {
-    #[Route('/htmltopdf', name: 'app_html_to_pdf')]
-    public function index(): Response
+    #[Route('/generateprescriptionpdf/{id}', name: 'app_htmltopdf_generate_prescription_pdf')]
+    public function generatePrescriptionPdf(Prescription $prescription): Response
     {
-        return $this->render('gestapp/htmltopdf/index.html.twig', [
-
-            // --- envoyer les variables du beneficiaire ---
-            'civility' => '',
-            'lastname' => '',
-            'firstname' => '',
-            'gender' => '',
-            'AgeGroup' => '',
-            'professionnalStatus' => '',
-
-            // --- envoyer les variables du prescripteur ---
-            'nameStructure' => '',
-            'contactResponsableLastname' => '',
-            'contactResponsableFirstname' => '',
-            'contactPhone' => '',
-            'contactEmail' => '',
-
-            // --- envoyer les variables de la date et du lieu de creation de la prescription ---
-            'lieuMediation' => '',
-            'createdAT' => '',
-            'matriculEquipment' => '',
-
-            // envoyer les variables booleenne des auto eva et autoevaend
-            'isAutoEva' => false,
-            'isAutoEvaEnd' => false,
+        return $this->render('gestapp/htmltopdf/prescriptionpdf.html.twig', [
+            'prescription' => $prescription,
         ]);
     }
 }
