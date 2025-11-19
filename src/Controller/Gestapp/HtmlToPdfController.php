575754af2a75aedf9ecc563fb6cb596a825fa770
jefflearning40 <jfld791000@gmail.com>
2025-11-19 10:58:44 +0100

    fix HtmlToPdfController send values to html

--- src/Controller/Gestapp/HtmlToPdfController.php
+++ src/Controller/Gestapp/HtmlToPdfController.php
@@ -12,7 +12,30 @@ final class HtmlToPdfController extends AbstractController
     public function index(): Response
     {
         return $this->render('gestapp/htmltopdf/index.html.twig', [
-            'controller_name' => 'HtmlToPdfController',
+
+            // --- envoyer les variables du beneficiaire ---
+            'civility' => '',
+            'lastname' => '',
+            'firstname' => '',
+            'gender' => '',
+            'AgeGroup' => '',
+            'professionnalStatus' => '',
+
+            // --- envoyer les variables du prescripteur ---
+            'nameStructure' => '',
+            'contactResponsableLastname' => '',
+            'contactResponsableFirstname' => '',
+            'contactPhone' => '',
+            'contactEmail' => '',
+
+            // --- envoyer les variables de la date et du lieu de creation de la prescription ---
+            'lieuMediation' => '',
+            'createdAT' => '',
+            'matriculEquipment' => '',
+
+            // envoyer les variables booleenne des auto eva et autoevaend
+            'isAutoEva' => false,
+            'isAutoEvaEnd' => false,
         ]);
     }
 }
